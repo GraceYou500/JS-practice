8a2a60533b27cef02f82a0a5b7017cb1d6a02a89
Grace You <graceyouya500@gmail.com>
2022-04-30 17:38:27 +1000

    Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,26 +1,85 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Martin";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Martin";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let myFirstJob = "Coder";
-let myCurrentJob = "Programmer";
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Programmer";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
 
-// Assignment
+// // Assignment
 
-let country = "China";
-let continent = "Asia";
-let population = "1300mil";
+// let country = "China";
+// let continent = "Asia";
+// let population = "1300mil";
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// let JavascriptIsFun = true;
+// console.log(JavascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof JavascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonase");
+
+// JavascriptIsFun = "YES!";
+// console.log(typeof JavascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1992;
+// console.log(typeof year);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1992;
+// birthYear = 1991;
+
+// const Job;
+
+var job = "accounting";
+job = "Coder";
+
+//math operators
+const now = 2037;
+const ageGrace = now - 1991;
+const ageTiff = now - 2019;
+console.log(ageGrace, ageTiff);
+
+console.log(ageGrace * 2, ageGrace / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 *2* 2
+
+const firstName = "Jonas";
+const lastName = "Schm";
+console.log(firstName + " " + lastName);
+
+//Assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 25 * 4
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+//comparision operators
+console.log(ageGrace > ageTiff); //., <, >=, <=
+console.log(ageTiff >= 18);
+
+const isFullAge = ageTiff >= 18;
+
+console.log(now - 1991 > now - 2018);
