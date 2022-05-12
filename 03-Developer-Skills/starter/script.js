d4bf4de9a02b74e1be5a22c94525be2f76bb1f31
Grace You <graceyouya500@gmail.com>
2022-05-12 22:59:55 +1000

    dev environment

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log();
+console.log();
+console.log(x);
+console.log(calcAge(1996));
