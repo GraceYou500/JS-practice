71ac64af520d33bae16d41a1894badef3140e39e
Grace You <graceyouya500@gmail.com>
2022-05-26 15:36:15 +1000

    logical siignment operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -58,13 +58,47 @@ const restaurant = {
   },
 };
 
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
-console.log('here2', guests);
+const rest1 = {
+  name: 'carpri',
+  // numGuests: 20,
+  numGuests: 0,
+};
 
-// Nullish: null and undefined (NOT include 0 or empty string "")
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Givanni Rossi',
+};
+
+//OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+//nullish assignment operator (null or undefined )
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+/////////////////////////////////////////////////
+//The Nullish Coalescing Operator
+
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log('here2', guests);
+
+// // Nullish: null and undefined (NOT include 0 or empty string "")
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
 
 // ////////////////////////////////////////////////////////////
 // //Short Circuiting (&& and ||)
