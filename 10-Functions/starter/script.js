9f8e2c888be056c67050f693964933af515c54ee
Grace You <graceyouya500@gmail.com>
2022-05-31 21:08:52 +1000

    function argument

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,65 @@
 'use strict';
+
+// Default parrameter
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
+
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+
+// createBooking('LH123', undefined, 1000);
+
+// Argument works
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schem',
+  passport: 345634536,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 345634536) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+const flightNum = flight; // two different address, will not affect original value, just copy a new one
+const passenger = jonas; // they both are same object in the memory heap. for object, what changes hapenned in copy, will also happen in original.
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
