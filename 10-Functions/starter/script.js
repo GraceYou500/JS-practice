0535c26c3b0661897940617909ac37153596ec18
Grace You <graceyouya500@gmail.com>
2022-05-31 22:17:15 +1000

    higher level function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -28,38 +28,67 @@
 
 // createBooking('LH123', undefined, 1000);
 
-// Argument works
+// // Argument works
 
-const flight = 'LH234';
-const jonas = {
-  name: 'Jonas Schem',
-  passport: 345634536,
-};
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schem',
+//   passport: 345634536,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
 
-  if (passenger.passport === 345634536) {
-    alert('Check in');
-  } else {
-    alert('Wrong passport');
-  }
-};
+//   if (passenger.passport === 345634536) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
 
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // Is the same as doing...
+// const flightNum = flight; // two different address, will not affect original value, just copy a new one
+// const passenger = jonas; // they both are same object in the memory heap. for object, what changes hapenned in copy, will also happen in original.
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000);
+// };
+
+// newPassport(jonas);
 // checkIn(flight, jonas);
 // console.log(flight);
 // console.log(jonas);
 
-// Is the same as doing...
-const flightNum = flight; // two different address, will not affect original value, just copy a new one
-const passenger = jonas; // they both are same object in the memory heap. for object, what changes hapenned in copy, will also happen in original.
-
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 1000000000);
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 };
 
-newPassport(jonas);
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transform string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('✌');
+};
+
+document.body.addEventListener('click', high5);
+['Jonas', 'Marta', 'Adam'].forEach(high5);
