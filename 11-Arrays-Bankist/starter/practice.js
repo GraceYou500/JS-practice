68f6c825d02cd8442d6b63f90bd8c4d41cc46a62
Grace You <graceyouya500@gmail.com>
2022-06-07 22:54:49 +1000

    method practice

--- 11-Arrays-Bankist/starter/practice.js
+++ 11-Arrays-Bankist/starter/practice.js
@@ -46,11 +46,11 @@ const dogs2 = [16, 6, 10, 5, 6, 1, 4];
 
 // Challenge 3 - change 2 into arrow function and chainning
 
-const calcAverageHumanAge = dogAges =>
-  dogAges
-    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
-    .filter(age => age >= 18)
-    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+// const calcAverageHumanAge = dogAges =>
+//   dogAges
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age >= 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 
-console.log(calcAverageHumanAge(dogs1));
-console.log(calcAverageHumanAge(dogs2));
+// console.log(calcAverageHumanAge(dogs1));
+// console.log(calcAverageHumanAge(dogs2));
