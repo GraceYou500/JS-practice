01d7ed4d16bb31711ab02740134f0134e9ce9448
Grace You <graceyouya500@gmail.com>
2022-06-08 17:28:33 +1000

    array challenge

--- 11-Arrays-Bankist/starter/practice.js
+++ 11-Arrays-Bankist/starter/practice.js
@@ -54,3 +54,82 @@ const dogs2 = [16, 6, 10, 5, 6, 1, 4];
 
 // console.log(calcAverageHumanAge(dogs1));
 // console.log(calcAverageHumanAge(dogs2));
+
+// Chalenge 4
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1.
+const reconmendFood = dogs.map(
+  dog => (dog.recommendFood = Math.trunc(dog.weight ** 0.75 * 28))
+);
+
+console.log(dogs);
+
+// 2.
+const dogSarah = dogs.find(acc => acc.owners.includes('Sarah'));
+console.log(dogSarah);
+
+console.log(
+  `Sarah's dog eating too ${
+    dogSarah.curFood > dogSarah.recommendFood * 1.1 ? 'much' : 'little'
+  }`
+);
+
+// 3.
+
+const dogsEatTooMuch = dogs.filter(
+  (_, i) => dogs[i].curFood > dogs[i].recommendFood * 1.1
+);
+const ownersEatTooMuch = dogsEatTooMuch
+  .filter((_, i) => dogsEatTooMuch[i].owners)
+  .flatMap(dog => dog.owners);
+
+console.log(ownersEatTooMuch);
+
+// const ownersEatTooLittle = dogs.filter(
+//   (_, i) => dogs[i].curFood < dogs[i].recommendFood * 0.9
+// );
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recommendFood)
+  .flatMap(dog => dog.owners);
+
+console.log(ownersEatTooLittle);
+
+// 4.
+console.log(`${ownersEatTooMuch.join(' and ')}'s dog eating too much`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dog eating too little`);
+
+// 5.
+console.log(dogs.some(dog => dog.curFood === dog.recommendFood));
+
+// 6.
+console.log(
+  dogs.some(
+    dog =>
+      dog.curFood > dog.recommendFood * 0.9 &&
+      dog.curFood < dog.recommendFood * 1.1
+  )
+);
+
+// 7.
+const dogsEatOK = dogs.filter(
+  dog =>
+    dog.curFood > dog.recommendFood * 0.9 &&
+    dog.curFood < dog.recommendFood * 1.1
+);
+console.log(dogsEatOK);
+
+// 8.
+
+const dgosAcending = dogs
+  .slice()
+  .sort((a, b) => a.recommendFood - b.recommendFood);
+console.log(dgosAcending);
+console.log(dogs);
