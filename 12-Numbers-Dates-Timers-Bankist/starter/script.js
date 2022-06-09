ef6bae1039b99f2cc0eac3ac1511f796dd152afe
Grace You <graceyouya500@gmail.com>
2022-06-09 21:36:39 +1000

    timer countdown

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -186,15 +186,46 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+// Set Timer
+
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, '0');
+    const sec = String(Math.trunc(time % 60)).padStart(2, '0');
+
+    // In each call, print the remaining time to the UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+    // When 0 seconds, stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = 'Log in to get started';
+      containerApp.style.opacity = 0;
+    }
+
+    //Decrease one second
+    time--;
+  };
+
+  // Set time to 5 mins
+  let time = 130;
+
+  // Call the timer wvery second
+  tick();
+  const timerDown = setInterval(tick, 1000);
+
+  return timerDown;
+};
+
 ///////////////////////////////////////
 
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
 
-// FAKE ALWAYS LOGGED IN
-currentAccount = account1;
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
+// // FAKE ALWAYS LOGGED IN
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -243,6 +274,10 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
+    // Timer
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -272,6 +307,10 @@ btnTransfer.addEventListener('click', function (e) {
 
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
@@ -290,6 +329,10 @@ btnLoan.addEventListener('click', function (e) {
 
       // Update UI
       updateUI(currentAccount);
+
+      // Reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
     }, 2500);
   }
   inputLoanAmount.value = '';
@@ -565,3 +608,12 @@ if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
 //   const now = new Date();
 //   console.log(now);
 // }, 3000);
+{
+  const fun1 = function () {
+    console.log(AAA);
+  };
+
+  const AAA = 'aaa';
+
+  fun1();
+}
