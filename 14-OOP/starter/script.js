564ca06d1527ab6904cb2af12863e61f73738265
Grace You <graceyouya500@gmail.com>
2022-06-20 17:07:41 +1000

    getter and setter

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -111,3 +111,78 @@ bmw.brake();
 bmw.accelerate();
 
 const tesla = { make: 'tesla', speed: 122 };
+
+// ES6 classes
+
+// Class expression
+// const PersonCl1 = class {};
+
+// Class declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  } // the method in prototype not the object themselves.
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+
+jessica.greet();
+
+// 1. Classes are Not hoisted ( cannot use them before declared)
+// 2. Classes are first-class citizen ( we can pass them into functions, and also return them from functions)
+// 3. Classes are execute in strict mode
+
+const walter = new PersonCl('Walter White', 1965);
+console.log(walter);
+
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
