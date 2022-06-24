bf802fc16429adff131b93f57c7a4d40b369ea3b
Grace You <graceyouya500@gmail.com>
2022-06-24 18:09:49 +1000

    Geolocation API

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -247,30 +247,30 @@ class PersonCl {
 
 // ES6 Classes
 
-// class StudentCl extends PersonCl {
-//   constructor(fullName, birthYear, course) {
-//     // Always needs to happen first!
-//     super(fullName, birthYear);
-//     this.course = course;
-//   }
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Always needs to happen first!
+    super(fullName, birthYear);
+    this.course = course;
+  }
 
-//   introduce() {
-//     console.log(`My name is ${this.fulltName} and study ${this.course}`);
-//   }
+  introduce() {
+    console.log(`My name is ${this.fulltName} and study ${this.course}`);
+  }
 
-//   calcAge() {
-//     console.log(
-//       `i am ${
-//         2037 - this.birthYear
-//       } years old, but as a student I feel more like ${
-//         2037 - this.birthYear + 10
-//       }`
-//     );
-//   }
-// }
+  calcAge() {
+    console.log(
+      `i am ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
 
-// const Martah = new StudentCl('marta Jonas', 2012);
-// const Marta = new StudentCl('marta Jonas', 2012, 'computer');
+const Martah = new StudentCl('marta Jonas', 2012);
+const Marta = new StudentCl('marta Jonas', 2012, 'computer');
 
 // console.log(Martah.age);
 // Martah.calcAge();
