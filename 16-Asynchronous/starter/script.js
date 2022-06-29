45bc0731363a1df97153296b130856d99ada9d7f
Grace You <graceyouya500@gmail.com>
2022-06-29 23:21:15 +1000

    chain promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -71,51 +71,95 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbour = function (country) {
-  // AJAX call country 1
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
+// const getCountryAndNeighbour = function (country) {
+//   // AJAX call country 1
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', function () {
-    // as soon as the data arravied, the callback function will be called
-    // console.log(this.responseText);
+//   request.addEventListener('load', function () {
+//     // as soon as the data arravied, the callback function will be called
+//     // console.log(this.responseText);
 
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    // Render country 1
-    renderCountry(data);
+//     // Render country 1
+//     renderCountry(data);
 
-    // Get Neibour country 2
-    const [neighbour] = data.borders;
+//     // Get Neibour country 2
+//     const [neighbour] = data.borders;
 
-    if (!neighbour) return;
+//     if (!neighbour) return;
 
-    // AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
-    request2.send();
+//     // AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+//     request2.send();
 
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-      console.log(data2);
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
 
-      renderCountry(data2, 'neighbour');
-    });
-  });
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// // getCountryAndNeighbour('Portugal');
+// // getCountryAndNeighbour('usa');
+// getCountryAndNeighbour('china');
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 second passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+///////////////////////////////////////////////////////////////////////
+
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+// Fetch API
+const request = fetch('https://restcountries.com/v2/name/china');
+console.log(request);
+
+// Consume promise
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`) // calling fetch function like this, will immideately return a promise. as soon as we start the request.  pending -> settle
+//     .then(function (
+//       response // then method can get response of the fetch method (handle the promise). response here is the resulted value.
+//     ) {
+//       return response.json(); // read response, it also return a promise.
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]); // take the data to render to the DOM.
+//     });
+// };
+
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
-// getCountryAndNeighbour('Portugal');
-// getCountryAndNeighbour('usa');
-getCountryAndNeighbour('china');
-
-setTimeout(() => {
-  console.log('1 second passed');
-  setTimeout(() => {
-    console.log('2 second passed');
-    setTimeout(() => {
-      console.log('3 second passed');
-    }, 1000);
-  }, 1000);
-}, 1000);
+getCountryData('portugal');
